86baf438d54808112e36f7bcd571001962647a0d
kuper0201 <kuper171@gmail.com>
2024-07-15 17:43:20 +0900

    Update NVMon.py

--- src/NVMon.py
+++ src/NVMon.py
@@ -61,7 +61,7 @@ class NVMon(Tk):
         self.power_data['text'] = '{}W / {}W'.format(powerDraw, powerLimit)
         self.update()
 
-    def resource_path(relative_path):
+    def resource_path(self, relative_path):
         if hasattr(sys, '_MEIPASS'):
             return os.path.join(sys._MEIPASS, relative_path)
         return os.path.join(os.path.abspath("."), relative_path)
